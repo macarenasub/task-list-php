3d86edd4d3279acb54dbb1851c467a4ec4a2ab23
Macarena Ugarte <macarenasub@gmail.com>
2019-06-04 21:01:41 -0300

    JAWSDB_MARIA db

--- db.php
+++ db.php
@@ -2,10 +2,14 @@
 
 session_start();
 
-$conn = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'makiapp'
-);
+$url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+$conn = new mysqli($hostname, $username, $password, $database);
+
 ?>
