455defc3544b537b8997ab997905cca2c6096030
Macarena Ugarte <macarenasub@gmail.com>
2019-06-04 21:03:12 -0300

    Cambios conexión db

--- db.php
+++ db.php
@@ -10,6 +10,9 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'],'/');
 
-$conn = new mysqli($hostname, $username, $password, $database);
+$conn = mysqli_connect($hostname, $username, $password, $database);
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
